6e20fca3eb5f9c92be1d0a559f14379fb03438ff
Stepan <38866661+STEPANONEDEATH@users.noreply.github.com>
2025-04-25 14:33:17 +0500

    Delete Stock_market directory
    delete last version

--- Stock_market/init_db.py
+++
@@ -1,9 +0,0 @@
-from database import Base, engine
-from schemas import User, Instrument, Order, Balance, Transaction
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
-    print("Таблицы успешно созданы!")
-
-if __name__ == "__main__":
-    init_db()
