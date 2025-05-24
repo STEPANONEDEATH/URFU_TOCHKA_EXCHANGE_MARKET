abaa003e7ccc7258dbc2e5b9a16ffb2c999156cd
STEPANONEDEATH <stepan.sidorov.2005@gmail.com>
2025-05-24 11:36:20 +0300

    Some last updates

--- Stock_market/routers/public.py
+++ Stock_market/routers/public.py
@@ -12,11 +12,12 @@ router = APIRouter(tags=["public"])
     "/register",
     response_model=User,
     summary="Register",
-    description="""Регистрация пользователя в платформе. Обязательна для совершения сделок.
-                              api_key полученный из этого метода следует передавать в другие через заголовок Authorization.
-            
-                              Например для api_key='key-bee6de4d-7a23-4bb1-a048-523c2ef0ea0c` значение будет таким:
-                              Authorization: TOKEN key-bee6de4d-7a23-4bb1-a048-523c2ef0ea0c""",
+    description=(
+        "Регистрация пользователя в платформе. Обязательна для совершения сделок. "
+        "api_key полученный из этого метода следует передавать в другие через заголовок Authorization. "
+        "Например для api_key='key-bee6de4d-7a23-4bb1-a048-523c2ef0ea0c` значение будет таким: "
+        "Authorization: TOKEN key-bee6de4d-7a23-4bb1-a048-523c2ef0ea0c"
+    )
 )
 def register(new_user: NewUser, db: Session = Depends(get_db)):
     return create_user(db, new_user)
@@ -26,7 +27,9 @@ def register(new_user: NewUser, db: Session = Depends(get_db)):
     "/instrument",
     response_model=list[Instrument],
     summary="List Instruments",
-    description="""Список доступных инструментов""",
+    description=(
+        "Список доступных инструментов"
+    )
 )
 def list_instruments(db: Session = Depends(get_db)):
     return get_instruments(db)
@@ -36,7 +39,9 @@ def list_instruments(db: Session = Depends(get_db)):
     "/orderbook/{ticker}",
     response_model=L2OrderBook,
     summary="Get Orderbook",
-    description="""Текущие заявки""",
+    description=(
+        "Текущие заявки"
+    )
 )
 def get_orderbook_endpoint(
     ticker: str, limit: int = Query(10, le=25), db: Session = Depends(get_db)
@@ -65,7 +70,9 @@ def get_orderbook_endpoint(
     "/transactions/{ticker}",
     response_model=list[Transaction],
     summary="Get Transaction History",
-    description="""История сделок""",
+    description=(
+        "История сделок"
+    )
 )
 def get_transaction_history(
     ticker: str, limit: int = Query(10, le=100), db: Session = Depends(get_db)
